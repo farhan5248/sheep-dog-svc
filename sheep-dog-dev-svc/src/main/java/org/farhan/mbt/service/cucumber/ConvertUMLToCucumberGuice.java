bf47f47d75515d7579b06315425d9d90896bedc4
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2026-03-28 22:04:15 -0400

    Split sheep-dog-dev-svc into api and code-gen services

--- sheep-dog-dev-svc/src/main/java/org/farhan/mbt/service/cucumber/ConvertUMLToCucumberGuice.java
+++ sheep-dog-dev-svc/src/main/java/org/farhan/mbt/service/cucumber/ConvertUMLToCucumberGuice.java
@@ -1,24 +1,10 @@
 package org.farhan.mbt.service.cucumber;
 
 import org.farhan.dsl.grammar.IResourceRepository;
-import org.farhan.mbt.core.UMLTestProject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-public class ConvertUMLToCucumberGuice extends ConvertUMLToCucumber {
+public class ConvertUMLToCucumberGuice extends org.farhan.mbt.cucumber.ConvertUMLToCucumberGuice {
 
 	public ConvertUMLToCucumberGuice(String tags, IResourceRepository fa, String serverHost, int serverPort) {
-		super(tags, fa, serverHost, serverPort);
-	}
-
-	public void initProjects() throws Exception {
-		if (model != null && project != null) {
-			return;
-		}
-		model = new UMLTestProject(this.tags, this.fa);
-		project = new CucumberGuiceTestProject(this.tags, this.fa);
-		model.init();
-		project.init();
-		this.pathConverter = new CucumberPathConverter(model, (CucumberTestProject) project);
+		super(tags, fa);
 	}
 }
--- sheep-dog-dev-svc/src/main/java/org/farhan/mbt/service/cucumber/ConvertUMLToCucumberSpring.java
+++ sheep-dog-dev-svc/src/main/java/org/farhan/mbt/service/cucumber/ConvertUMLToCucumberSpring.java
@@ -1,25 +1,10 @@
 package org.farhan.mbt.service.cucumber;
 
 import org.farhan.dsl.grammar.IResourceRepository;
-import org.farhan.mbt.core.UMLTestProject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-public class ConvertUMLToCucumberSpring extends ConvertUMLToCucumber {
+public class ConvertUMLToCucumberSpring extends org.farhan.mbt.cucumber.ConvertUMLToCucumberSpring {
 
 	public ConvertUMLToCucumberSpring(String tags, IResourceRepository fa, String serverHost, int serverPort) {
-		super(tags, fa, serverHost, serverPort);
+		super(tags, fa);
 	}
-
-	public void initProjects() throws Exception {
-		if (model != null && project != null) {
-			return;
-		}
-		model = new UMLTestProject(this.tags, this.fa);
-		project = new CucumberSpringTestProject(this.tags, this.fa);
-		model.init();
-		project.init();
-		this.pathConverter = new CucumberPathConverter(model, (CucumberTestProject) project);
-	}
-
 }
