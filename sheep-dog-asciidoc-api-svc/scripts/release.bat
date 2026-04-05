8256c7ad73023f8a870af5cfa7885ce316a9913a
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2026-04-05 00:59:10 -0400

    Make single release workflow per repo

--- sheep-dog-asciidoc-api-svc/scripts/release.bat
+++ sheep-dog-asciidoc-api-svc/scripts/release.bat
@@ -1,13 +1,13 @@
 cd ..
-call git reset --hard HEAD
-call git clean -fdx
-call git pull
-call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=false -DallowDowngrade=true
-call mvn org.farhan:sheep-dog-mgmt-maven-plugin:prepare -DpreparationGoals=deploy,-DskipTests
-call git push
-call git push --tags
-call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=true
-call git clean -fdx
+call git reset --hard HEAD || exit /b 1
+call git clean -fdx || exit /b 1
+call git pull || exit /b 1
+call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=false -DallowDowngrade=true || exit /b 1
+call mvn org.farhan:sheep-dog-mgmt-maven-plugin:prepare -DpreparationGoals=deploy,-DskipTests || exit /b 1
+call git push || exit /b 1
+call git push --tags || exit /b 1
+call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=true || exit /b 1
+call git clean -fdx || exit /b 1
 git diff --quiet HEAD -- . ":(exclude)*.bat" && (echo No dependency changes to commit) || (call git add * && call git commit -m "Upgrading dependency versions" && call git push)
-call mvn clean deploy -DskipTests
+call mvn clean deploy -DskipTests || exit /b 1
 cd scripts
