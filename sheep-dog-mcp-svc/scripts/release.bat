e39d2e5804acc34ab8d1a3dc3dc4be4d493418ab
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2026-04-04 21:19:59 -0400

    Make single release workflow per repo

--- sheep-dog-mcp-svc/scripts/release.bat
+++ sheep-dog-mcp-svc/scripts/release.bat
@@ -1,10 +1,13 @@
 cd ..
+call git reset --hard HEAD
+call git clean -fdx
+call git pull
 call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=false -DallowDowngrade=true
-call mvn -B release:clean release:prepare -DcheckModificationExcludeList=pom.xml -DpreparationGoals=deploy
+call mvn org.farhan:sheep-dog-mgmt-maven-plugin:prepare -DpreparationGoals=deploy,-DskipTests
 call git push
+call git push --tags
 call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DallowSnapshots=true
-call git add *
-call git commit -m "Upgrading dependency versions"
-call git push
-call mvn deploy -DskipTests
+call git clean -fdx
+git diff --quiet HEAD -- . ":(exclude)*.bat" && (echo No dependency changes to commit) || (call git add * && call git commit -m "Upgrading dependency versions" && call git push)
+call mvn clean deploy -DskipTests
 cd scripts
