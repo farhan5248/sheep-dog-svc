e371b1e96331a1e28f04a5ca195deeb687f2f5c2
Farhan Sheikh <farhan.sheikh.5248@gmail.com>
2026-04-15 21:32:22 -0400

    Refactor TestObject: extract cursor helpers into PathNavigatorTestObject subclass

--- sheep-dog-svc-maven-plugin/src/test/java/org/farhan/impl/TestObjectSheepDogImpl.java
+++ sheep-dog-svc-maven-plugin/src/test/java/org/farhan/impl/TestObjectSheepDogImpl.java
@@ -3,9 +3,4 @@ package org.farhan.impl;
 import org.farhan.common.TestObject;
 
 public abstract class TestObjectSheepDogImpl extends TestObject {
-
-	@Override
-	protected Object getOrCreateNode(Object parent, String elementType, int index) {
-		return null;
-	}
 }
